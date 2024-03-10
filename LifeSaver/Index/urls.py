1b9c5e5c2c23f30abb275b0fe91c2577765b7587
harshitajain.4063@gmail.com <vireshshah17062004@gmail.com>
2024-03-10 12:02:21 +0530

    mini updates

--- LifeSaver/Index/urls.py
+++ LifeSaver/Index/urls.py
@@ -5,5 +5,6 @@ app_name = 'index'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact , name='contact'),
-    path('login', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- LifeSaver/Index/views.py
+++ LifeSaver/Index/views.py
@@ -9,3 +9,6 @@ def contact(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
