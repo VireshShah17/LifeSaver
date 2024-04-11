5648100982f45e51992dc901922a5678ac3186d7
harshitajain.4063@gmail.com <vireshshah17062004@gmail.com>
2024-04-11 16:36:19 +0530

    Working of the donor and receiver button
    1. Created 2 views in view.py of Index
    2. Added urls for the new page
    3. Created 2 new templates for donor and receiver

--- Index/urls.py
+++ Index/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'index'
 urlpatterns = [
    path('', view=views.index, name='index'),
+   path('donor/', view=views.donor_view, name='donor'),
+   path('receiver/', view=views.receiver_view, name='receiver'),
 ]
--- Index/views.py
+++ Index/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def donor_view(request):
+    return render(request, "donor.html")
+
+
+def receiver_view(request):
+    return render(request, 'receiver.html')
